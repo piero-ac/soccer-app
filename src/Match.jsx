819986455b6e23d5702b19890f51b3ae8d9c3696
Piero Coronado <pacoronado01@gmail.com>
2023-06-19 09:15:27 -0400

    Create match component

---
+++ src/Match.jsx
@@ -0,0 +1,8 @@
+
+export default function Match(props) {
+  return (
+    <div className="row align-items-start justify-content-between border border-secondary rounded match">
+      {props.children}
+    </div>
+  )
+}
