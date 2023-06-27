2a73bb3cc304153fa6cb383fb67009534c214aeb
Piero Coronado <pacoronado01@gmail.com>
2023-06-27 19:33:46 -0400

    Handle error message about whitespace text nodes in tbody

--- src/LeagueTable.jsx
+++ src/LeagueTable.jsx
@@ -3,7 +3,6 @@ import useRapidAPI from "./hooks/use-rapidapi";
 
 export default function LeagueTable(props) {
   const league = props.league, season = props.season;
-
   const [leagueTableData, setLeagueTableData] = useState([]);
   const {loading, error, sendRequest: fetchTableData} = useRapidAPI();
 
@@ -44,77 +43,40 @@ export default function LeagueTable(props) {
   let content = '';
 
   if(leagueTableData.length > 0) {
-    console.log(leagueTableData)
-    content = leagueTableData.map(team => {
-      let rowClasses = '';
-      if (team.teamRank === 1) rowClasses += "table-success";
-      else if (team.teamRank >= 2 && team.teamRank <= 17) rowClasses += "table-secondary";
-      else rowClasses += "table-danger";
-      return (
-        <tr key={team.teamId} className={rowClasses}>
-          <th scope="row">{team.teamRank}</th>
-          <td><img src={team.teamLogo} width="30px" height="auto" /></td>
-          <td className="text-left">{team.teamName}</td>
-          <td>{team.totalGamesPlayed}</td>
-          <td>{team.totalGamesWon}</td>
-          <td>{team.totalGamesDraw}</td>
-          <td>{team.totalGamesLose}</td>
-          <td>{team.totalGoalsFor}</td>
-          <td>{team.totalGoalsAgainst}</td>
-          <td>{team.totalGoalsDiff}</td>
-          <td>{team.totalPoints}</td>
-          <td>{team.teamForm}</td>
-        </tr>
-      );
-    })
+    content = (
+      <tbody className="table-group-divider">
+        {leagueTableData.map(team => {
+          let rowClasses = '';
+          if (team.teamRank === 1) rowClasses += "table-success";
+          else if (team.teamRank >= 2 && team.teamRank <= 17) rowClasses += "table-secondary";
+          else rowClasses += "table-danger";
+          return (
+            <tr key={team.teamId} className={rowClasses}>
+              <th scope="row">{team.teamRank}</th>
+              <td><img src={team.teamLogo} width="30px" height="auto" /></td>
+              <td className="text-left">{team.teamName}</td>
+              <td>{team.totalGamesPlayed}</td>
+              <td>{team.totalGamesWon}</td>
+              <td>{team.totalGamesDraw}</td>
+              <td>{team.totalGamesLose}</td>
+              <td>{team.totalGoalsFor}</td>
+              <td>{team.totalGoalsAgainst}</td>
+              <td>{team.totalGoalsDiff}</td>
+              <td>{team.totalPoints}</td>
+              <td>{team.teamForm}</td>
+            </tr>
+          );
+        })}
+      </tbody>)
   }
 
   if(loading) {
-    content = <tr><th colSpan="12">Loading...</th></tr>
+    content = <tbody className="table-group-divider"><tr><th colSpan="12">Loading...</th></tr></tbody>
   }
 
   if(error) {
-    content = <tr><th colSpan="12">Something went wrong...</th></tr>
+    content = <tbody className="table-group-divider"><tr><th colSpan="12">Something went wrong...</th></tr></tbody>
   }
-
-
-  // useEffect(() => {
-  //   const RAPID_API_KEY = import.meta.env.VITE_RAPID_API_KEY;
-  //   const key = `leaguetable-l=${league}-s=${season}`;
-
-  //   const fetchLeagueTableData = async () => {
-  //     let data;
-  //     const savedData = localStorage.getItem(key);
-
-  //     if (savedData) {
-  //       data = JSON.parse(savedData);
-  //       console.log("Using cached league table info");
-  //     } else {
-  //       const url = `https://api-football-v1.p.rapidapi.com/v3/standings?season=${season}&league=${league}`;
-  //       const options = {
-  //         method: 'GET',
-  //         headers: {
-  //           'X-RapidAPI-Key': RAPID_API_KEY,
-  //           'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-  //         }
-  //       };
-
-  //       try {
-  //         const response = await fetch(url, options);
-  //         const result = await response.json();
-  //         data = result.response[0].league.standings[0];
-  //         localStorage.setItem(key, JSON.stringify(data));
-  //         console.log('New league table info requested');
-  //       } catch (error) {
-  //         console.error(error);
-  //       }
-  //     }
-  //     setLeagueTableData(data || []);
-  //   };
-
-  //   fetchLeagueTableData();
-
-  // }, [league, season]);
 
   return (
     <>
@@ -135,7 +97,7 @@ export default function LeagueTable(props) {
               <th scope="col">Last 5</th>
             </tr>
           </thead>
-          <tbody className="table-group-divider">{content}</tbody>
+          {content}
         </table>
       </div>
     </>
