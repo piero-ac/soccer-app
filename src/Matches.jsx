ae8d024b4a601d4d3278c9bf0a6180fca20ab437
Piero Coronado <pacoronado01@gmail.com>
2023-06-27 20:40:18 -0400

    Refactor code to use custom hook

--- src/Matches.jsx
+++ src/Matches.jsx
@@ -2,52 +2,94 @@ import { useState, useEffect } from "react";
 import Match from "./Match";
 import MatchTeam from './MatchTeam';
 import MatchScore from './MatchScore';
+import useRapidAPI from "./hooks/use-rapidapi";
 
 export default function Matches(props) {
   const league = props.league, season = props.season;
   const [currentMatchday, setCurrentMatchday] = useState('Regular Season - 1');
   const [matchesData, setMatchesData] = useState([]);
   const [leagueRoundsData, setLeagueRoundsData] = useState([]);
+  const {loading, error, sendRequest: fetchData} = useRapidAPI();
 
   useEffect(() => {
-    const RAPID_API_KEY = import.meta.env.VITE_RAPID_API_KEY;
     const matchesKey = `leaguematches-l=${league}-s=${season}`;
     const roundsKey = `leaguerounds-l=${league}-s=${season}`;
 
-    const fetchLeagueRoundsAndMatchesData = async (endpoint, key, setData) => {
-      let storedData = localStorage.getItem(key);
+    const savedMatchesData = localStorage.getItem(matchesKey);
+    const savedRoundsData = localStorage.getItem(roundsKey);
 
-      if (storedData) {
-        setData(JSON.parse(storedData));
-        console.log('Using cached data');
-      } else {
-        const url = `https://api-football-v1.p.rapidapi.com/v3/${endpoint}?league=${league}&season=${season}`;
-        const options = {
-          method: 'GET',
-          headers: {
-            'X-RapidAPI-Key': RAPID_API_KEY,
-            'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-          }
-        };
+    if(savedMatchesData) {
+      console.log("Using cached matches and rounds data");
+      setMatchesData(JSON.parse(savedMatchesData));
+      setLeagueRoundsData(JSON.parse(savedRoundsData));
+    } else {
+      const parseMatchesData = (dataObj) => {
+        const data = dataObj.response;
+        const parsedData = [];
 
-        try {
-          const response = await fetch(url, options);
-          const result = await response.json();
-          localStorage.setItem(key, JSON.stringify(result.response));
-          setData(result.response);
-          console.log('New data requested');
-        } catch (error) {
-          console.error(error);
+        for(let match of data){
+          parsedData.push({
+            round: match.league.round,
+            date: new Date(match.fixture.date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }),
+            id: match.fixture.id,
+            homeTeam: { logo: match.teams.home.logo, name: match.teams.home.name},
+            awayTeam: { logo: match.teams.away.logo, name: match.teams.away.name},
+            score: { home: match.score.fulltime.home, away: match.score.fulltime.away }
+          })
         }
-      }
+        localStorage.setItem(matchesKey, JSON.stringify(parsedData));
+        setMatchesData(parsedData);
+      };
+
+      const parseRoundsData = (dataObj) => {
+        const data = dataObj.response;
+        localStorage.setItem(roundsKey,JSON.stringify(data));
+        setLeagueRoundsData(data);
+      };
+
+      console.log("Fetching new matches and rounds data");
+      fetchData(`https://api-football-v1.p.rapidapi.com/v3/fixtures?league=${league}&season=${season}`, parseMatchesData);
+      fetchData(`https://api-football-v1.p.rapidapi.com/v3/fixtures/rounds?league=${league}&season=${season}`, parseRoundsData);
     }
 
-    fetchLeagueRoundsAndMatchesData('fixtures', matchesKey, setMatchesData);
-    fetchLeagueRoundsAndMatchesData('fixtures/rounds', roundsKey, setLeagueRoundsData);
-  }, [league, season]);
+  }, [league, season, fetchData]);
 
-  const filteredMatches = matchesData.filter(match => match.league.round === currentMatchday);
+  const filteredMatches = matchesData.filter(match => match.round === currentMatchday);
+  let content = '';
 
+  if(filteredMatches.length > 0) {
+    content = (
+      <div className="container text-center w-75 p-3">
+        {filteredMatches.length !== 0 && filteredMatches.map(match => {
+          return (
+            <Match key={match.id} >
+              <MatchTeam
+                teamLogo={match.homeTeam.logo}
+                teamName={match.homeTeam.name}
+              />
+              <MatchScore
+                homeTeamScore={match.score.home}
+                awayTeamScore={match.score.away}
+                matchdate={match.date}
+              />
+              <MatchTeam
+                teamLogo={match.awayTeam.logo}
+                teamName={match.awayTeam.name}
+              />
+            </Match>
+          );
+        })}
+      </div>
+    );
+  }
+
+  if(loading) {
+    content = <div>Loading...</div>
+  }
+
+  if(error) {
+    content = <div>Something went wrong...</div>
+  }
   return (
     <>
       <div className="form-container">
@@ -59,32 +101,7 @@ export default function Matches(props) {
           </select>
         </form>
       </div>
-      <div className="container text-center w-50 p-3">
-        {filteredMatches.length !== 0 && filteredMatches.map(match => {
-          const dateObj = new Date(match.fixture.date);
-          const options = { year: 'numeric', month: 'long', day: 'numeric' };
-          const formattedDate = dateObj.toLocaleDateString('en-US', options);
-
-          return (
-            <Match key={match.fixture.id} >
-              <MatchTeam
-                teamLogo={match.teams.home.logo}
-                teamName={match.teams.home.name}
-              />
-              <MatchScore
-                homeTeamScore={match.score.fulltime.home}
-                awayTeamScore={match.score.fulltime.away}
-                matchdate={formattedDate}
-              />
-              <MatchTeam
-                teamLogo={match.teams.away.logo}
-                teamName={match.teams.away.name}
-              />
-            </Match>
-          );
-        })}
-      </div>
+      {content}
     </>
-
   );
 }
