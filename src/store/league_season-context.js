c2d868511f69e70c5208186e11a9c3fb6daa5499
Piero Coronado <pacoronado01@gmail.com>
2023-06-28 19:57:21 -0400

    Create context for selected league and season

--- src/store/league_season-context.js
+++ src/store/league_season-context.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 const LeagueSeasonContext = React.createContext({
-	league: "39",
-	season: "2022",
+	league: "39", // default league
+	season: "2022", // default season
+	seasonChangeHandler: () => {},
+	leagueChangeHandler: () => {},
 });
+
+export default LeagueSeasonContext;
