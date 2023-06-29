6865ddb46b56d202dfe215253ab9f0aeab00682a
Piero Coronado <pacoronado01@gmail.com>
2023-06-28 20:17:34 -0400

    Move decision logic outside of App component's return statement

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,60 @@ function App() {
     setIsChangingLeague(false);
   }
 
+  // Decide forms to display
+  let leagueDisplayContent = "";
+  if(isChangingLeague) {
+    leagueDisplayContent = <SelectForm
+      formId={"league-form"}
+      for={"league"}
+      selectName={"league-select"}
+      selectId={"league-select"}    
+    />
+  } else {
+    leagueDisplayContent = <h1 
+      onClick={() => {
+        setIsChangingLeague(true);
+        setIsChangingLeagueSeason(false);
+      }} >
+      {leagueMappings[leagueData.league]}
+    </h1>
+  }
+
+  let seasonDisplayContent = "";
+  if(isChangingLeagueSeason) {
+    seasonDisplayContent = 
+    <SelectForm
+      formId={"league-season-form"}
+      for={"season"}
+      selectName={"league-season-select"}
+      selectId={"league-season-select"}    
+    />
+  } else {
+    seasonDisplayContent = <h3 
+      onClick={() => {
+        setIsChangingLeagueSeason(true);
+        setIsChangingLeague(false);
+      }}>
+      {`${leagueData.season}-${Number(leagueData.season) + 1}`}
+    </h3>
+  }
+
+  // Decide tab info to display
+  let mainContent = "";
+  switch(selectedTab){
+    case 'top-scorers':
+      mainContent = <TopScorers />
+      break;
+    case 'table':
+      mainContent = <LeagueTable />
+      break;
+    case 'matches' :
+      mainContent =  <Matches />
+      break;
+    default:
+      mainContent = <LeagueTable />
+  }
+
   return (
     <LeagueSeasonContext.Provider value={{
       league: leagueData.league, 
@@ -50,33 +104,11 @@ function App() {
     }}>
 
       <header>
-        {!isChangingLeague && <h1 onClick={() => setIsChangingLeague(true)} >{leagueMappings[leagueData.league]}</h1>}
-        {isChangingLeague && 
-        <SelectForm
-            formId={"league-form"}
-            for={"league"}
-            selectName={"league-select"}
-            selectId={"league-select"}    
-           />
-
-
-        }
-        {isChangingLeagueSeason &&
-        <SelectForm
-        formId={"league-season-form"}
-        for={"season"}
-        selectName={"league-season-select"}
-        selectId={"league-season-select"}    
-       />
-        }
-        {!isChangingLeagueSeason && <h3 onClick={() => setIsChangingLeagueSeason(true)}>{`${leagueData.season}-${Number(leagueData.season) + 1}`}</h3>}
+        {leagueDisplayContent}
+        {seasonDisplayContent}
         <Navbar activeTab={selectedTab} onTabClick={tabSelectionHandler} />
       </header>
-      <div className='main-content'>
-        {selectedTab === 'top-scorers' && <TopScorers />}
-        {selectedTab === 'table' && <LeagueTable />}
-        {selectedTab === 'matches' && <Matches />}
-      </div>
+      <div className='main-content'>{mainContent}</div>
     </LeagueSeasonContext.Provider>
   )
 }
