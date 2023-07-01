cca8d122a48a1005da528594705318651ce711ad
Piero Coronado <pacoronado01@gmail.com>
2023-06-30 21:05:52 -0400

    Edit layout of match score component

--- src/Matches/MatchScore.jsx
+++ src/Matches/MatchScore.jsx
@@ -1,8 +1,14 @@
 export default function MatchScore(props) {
+  const [month, day, year] = props.matchdate.split(' ');
+
   return (
-    <div className="col-3 align-self-center match-score">
-      <p className="score">{props.homeTeamScore}-{props.awayTeamScore}</p>
-      <p className="date">{props.matchdate}</p>
+    <div className="d-flex flex-column justify-content-center text-center flex-fill">
+      <div className="m-0 lh-sm fw-bold">
+        <div>{month}</div>
+        <div>{day.substring(0,1)}</div>
+        <div>{year}</div>
+      </div>
+      <div className="m-0 fs-4">{props.homeTeamScore}-{props.awayTeamScore}</div>
     </div>
   );
 }
