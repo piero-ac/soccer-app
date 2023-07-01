80c10ea5a242addea239f6ff83e23a0b5ff2992f
Piero Coronado <pacoronado01@gmail.com>
2023-06-30 21:05:26 -0400

    Edit styling rules for responsiveness

--- src/Matches/Match.jsx
+++ src/Matches/Match.jsx
@@ -1,8 +1,8 @@
 
 export default function Match(props) {
   return (
-    <div className="row align-items-start justify-content-between border border-secondary rounded match">
-      {props.children}
+    <div className="col-12 col-md-5 mx-0 mb-2 border-top bg-primary-subtle border-bottom border-primary border-2 rounded-1 shadow-lg d-flex justify-content-between"> 
+        {props.children}
     </div>
   )
 }
--- src/Matches/MatchTeam.jsx
+++ src/Matches/MatchTeam.jsx
@@ -1,10 +1,8 @@
 export default function MatchTeam(props) {
   return (
-    <div className="col-4 align-self-center match-team">
-      <div width="50px" height="50px">
-        <img width="50px" height="50px" src={props.teamLogo} />
-      </div>
-      <p>{props.teamName}</p>
+    <div className="match-team d-flex flex-column align-items-center text-bg-primary rounded-3 my-1">
+      <img width="50px" height="50px" src={props.teamLogo} />
+      <p className="mt-1 mb-0 flex-fill text-center fw-bold" style={{fontSize: '0.8rem'}}>{props.teamName}</p>
     </div>
   )
 }
