96e87caa42eeed181841c1195b9dbabd3ccf0101
Piero Coronado <pacoronado01@gmail.com>
2023-06-30 21:29:45 -0400

    Edit styling to vertical and horizontally center content

--- src/Matches/MatchTeam.jsx
+++ src/Matches/MatchTeam.jsx
@@ -1,8 +1,9 @@
 export default function MatchTeam(props) {
   return (
-    <div className="match-team d-flex flex-column align-items-center text-bg-primary rounded-3 my-1">
-      <img width="50px" height="50px" src={props.teamLogo} />
-      <p className="mt-1 mb-0 flex-fill text-center fw-bold" style={{fontSize: '0.8rem'}}>{props.teamName}</p>
+    <div className="match-team text-bg-primary rounded-3 my-1">
+      <div className="h-50 text-center"><img width="50px" height="50px" src={props.teamLogo} /></div>
+      <div className="mt-1 mb-0 flex-fill text-center fw-bold h-50 d-flex align-items-center justify-content-center" style={{fontSize: '0.8rem'}}>
+        {props.teamName} </div>
     </div>
   )
 }
