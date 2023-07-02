edb8bcb6100a128419407d33c06f305784479d0f
Piero Coronado <pacoronado01@gmail.com>
2023-07-02 19:38:06 -0400

    Pass title prop to ModalOverlay

--- src/Matches/MatchModal.jsx
+++ src/Matches/MatchModal.jsx
@@ -2,22 +2,24 @@ import { Fragment } from 'react';
 import ReactDOM  from 'react-dom';
 import Card from '../UI/Card';
 import classes from './MatchModal.module.css';
+import MatchEvents from './MatchEvents';
 
 const Backdrop = (props) => {
   return <div className={classes.backdrop} onClick={(event) => {event.stopPropagation(); props.onClose();}} />;
 }
 
 const ModalOverlay = (props) => {
+  
   return (
     <Card className={classes.modal} >
       <header className="text-bg-primary p-1 m-0" onClick={props.onClose}>
-        <h2 className='fs-5 mt-2'>{props.matchId}</h2>
+        <h2 className='fs-5 mt-2'>{props.title}</h2>
       </header>
       <div className="p-2">
-        <p>Lorem ipsum dolor sit amet.</p>
+        <MatchEvents matchId={props.matchId}/>
       </div>
       <footer className="d-flex justify-content-end p-1">
-      <button className="btn btn-primary"onClick={(event) => {event.stopPropagation(); props.onClose();}}>Close</button>
+        <button className="btn btn-primary"onClick={(event) => {event.stopPropagation(); props.onClose();}}>Close</button>
       </footer>
     </Card>
   );
@@ -30,6 +32,7 @@ const MatchModal = (props) => {
        document.getElementById('backdrop-root'))}
       {ReactDOM.createPortal(<ModalOverlay 
       matchId={props.matchId}
+      title={props.title}
       onClose={props.onClose}/>, 
       document.getElementById('overlay-root'))} 
     </Fragment>
