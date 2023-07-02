f4ee3ec6660f06023a9fdd5c1ef2dfe53a2434bd
Piero Coronado <pacoronado01@gmail.com>
2023-07-02 15:15:28 -0400

    Add state for displaying match modal

--- src/Matches/Match.jsx
+++ src/Matches/Match.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
+import MatchModal from "./MatchModal";
 
 export default function Match(props) {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div className="col-12 col-md-5 mx-0 mb-2 border-top bg-primary-subtle border-bottom border-primary border-2 rounded-1 shadow-lg d-flex justify-content-between"> 
+    <div className="col-12 col-md-5 mx-0 mb-2 border-top bg-primary-subtle border-bottom border-primary border-2 rounded-1 shadow-lg d-flex justify-content-between"
+    onClick={() => setShowModal(true)}> 
         {props.children}
+    {showModal && <MatchModal matchId={props.matchId} onClose={() => setShowModal(false)}/>}
     </div>
   )
 }
