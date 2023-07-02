baaed0049cb8e9c32b7bc9bddf8fa573d1442c48
Piero Coronado <pacoronado01@gmail.com>
2023-07-02 19:36:24 -0400

    Pass title prop to MatchModal

--- src/Matches/Match.jsx
+++ src/Matches/Match.jsx
@@ -3,12 +3,14 @@ import MatchModal from "./MatchModal";
 
 export default function Match(props) {
   const [showModal, setShowModal] = useState(false);
-
   return (
     <div className="col-12 col-md-5 mx-0 mb-2 border-top bg-primary-subtle border-bottom border-primary border-2 rounded-1 shadow-lg d-flex justify-content-between"
     onClick={() => setShowModal(true)}> 
         {props.children}
-    {showModal && <MatchModal matchId={props.matchId} onClose={() => setShowModal(false)}/>}
+    {showModal && <MatchModal 
+      matchId={props.matchId} 
+      title={props.title} 
+      onClose={() => setShowModal(false)}/>}
     </div>
   )
 }
