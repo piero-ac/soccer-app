dd18fd6653ea4edca8d12670003705c525cb7d5a
Piero Coronado <pacoronado01@gmail.com>
2023-07-04 10:43:07 -0400

    Edit border of rows

--- src/TopScorers/TopScorersRow.jsx
+++ src/TopScorers/TopScorersRow.jsx
@@ -3,7 +3,7 @@ export default function TopScorersRow(props) {
   const {player} = props;
 
   return (
-    <div className="row rounded-1 my-1 py-1 d-flex align-items-center shadow">
+    <div className="row border border-secondary border-2 rounded-1 my-1 py-1 d-flex align-items-center shadow-lg">
       <div className="col-2 col-sm-1 fw-bold">{player.rank}</div>
       <div className="col-5">{player.name}</div>
       <div className="d-none d-sm-block col-2"><img className="border border-secondary border-bottom rounded-1" src={player.photoURL} width="50px" height="auto" alt=""/></div>
