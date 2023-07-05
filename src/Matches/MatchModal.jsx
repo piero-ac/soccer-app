f03209c277b598ee7165ca83285ecded0d43c4cc
Piero Coronado <pacoronado01@gmail.com>
2023-07-05 10:33:17 -0400

    Display match lineups component in modal

--- src/Matches/MatchModal.jsx
+++ src/Matches/MatchModal.jsx
@@ -4,6 +4,7 @@ import Card from '../UI/Card';
 import classes from './MatchModal.module.css';
 import MatchEvents from './MatchEvents/MatchEvents';
 import MatchStatistics from './MatchStatistics/MatchStatistics';
+import MatchLineups from './MatchLineups/MatchLineups';
 
 const Backdrop = (props) => {
   return <div className={classes.backdrop} onClick={(event) => {event.stopPropagation(); props.onClose();}} />;
@@ -18,6 +19,7 @@ const ModalOverlay = (props) => {
         <button className="btn btn-sm btn-outline-light"onClick={(event) => {event.stopPropagation(); props.onClose();}}>Close</button>
       </header>
       <div className="p-2">
+        <MatchLineups matchId={props.matchId}/>
         <MatchEvents matchId={props.matchId}/>
         <MatchStatistics matchId={props.matchId}/>
       </div>
