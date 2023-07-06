74209f15b3a3e90a8e443e75140d52b31cdd41d2
Piero Coronado <pacoronado01@gmail.com>
2023-07-06 19:50:15 -0400

    Refactor code to use custom backend hook, change lineups state structure

--- src/Matches/MatchLineups/MatchLineups.jsx
+++ src/Matches/MatchLineups/MatchLineups.jsx
@@ -1,47 +1,44 @@
-import { useEffect, useState } from 'react';
-import useRapidAPI from '../../hooks/use-rapidapi';
-import TeamLineup from './TeamLineup';
+import { useEffect, useState } from "react";
+// import useRapidAPI from "../../hooks/use-rapidapi";
+import useBackend from "../../hooks/use-backend";
+import TeamLineup from "./TeamLineup";
 
 export default function MatchLineups(props) {
   const {matchId} = props;
-  const [matchLineupsData, setMatchLineupsData] = useState([]);
-  const {loading, error, sendRequest: fetchMatchLineupsData} = useRapidAPI();
+  const [matchLineupsData, setMatchLineupsData] = useState({team1: [], team2: []});
+  const {loading, error, sendRequest: fetchMatchLineupsData} = useBackend();
 
   useEffect(() => {
     const key = `lineups-mid=${matchId}`;
     const savedData = localStorage.getItem(key);
-    console.log('Applying use effect')
 
     if(savedData) {
       console.log(`Using cached match lineups info for ${matchId}`);
       const cachedData = JSON.parse(savedData);
       setMatchLineupsData(cachedData);
     } else {
-      const parseMatchLineupsData = (dataObj) => {
-        const data = dataObj.response;
-        const [team1, team2] = data;
-        const parsedData = [team1, team2];
-        localStorage.setItem(key, JSON.stringify(parsedData));
-        setMatchLineupsData(parsedData);
+      const setData = (data) => {
+        localStorage.setItem(key, JSON.stringify(data));
+        setMatchLineupsData(data);
       };
       console.log(`Fetching new match lineups info for ${matchId}`);
-      const endpoint = `https://api-football-v1.p.rapidapi.com/v3/fixtures/lineups?fixture=${matchId}`;
-      fetchMatchLineupsData(endpoint, parseMatchLineupsData);
+      fetchMatchLineupsData(`/soccer/match/${matchId}/lineups`, setData);
     }
     
   }, [matchId, setMatchLineupsData, fetchMatchLineupsData]);
-
-  let content = '';
-  if(matchLineupsData.length > 0){
+  
+  const {team1, team2} = matchLineupsData;
+  let content = "";
+  if(Object.keys(team1).length > 0 || Object.keys(team2).length > 0){
     content = <>
-      <TeamLineup teamLineup={matchLineupsData[0]} />
-      <TeamLineup teamLineup={matchLineupsData[1]} />
+      <TeamLineup teamLineup={matchLineupsData.team1} />
+      <TeamLineup teamLineup={matchLineupsData.team2} />
     </>
   }
 
   if(loading) {
     content = (
-    <div className='text-center mt-3'>
+    <div className="text-center mt-3">
       <div className="spinner-border text-primary" role="status">
         <span className="visually-hidden">Loading...</span>
       </div>
@@ -50,13 +47,13 @@ export default function MatchLineups(props) {
 
   if(error) {
     content = (
-      <div className='bg-danger text-center mt-3'>Could not fetch lineups.</div>
+      <div className="bg-danger text-center mt-3">Could not fetch lineups.</div>
     )
   }
   
   return (
     <div className="container">
-      <h6 className='text-center'>
+      <h6 className="text-center">
         <a className="fs-2 link-underline link-underline-opacity-0" 
           data-bs-toggle="collapse" 
           href="#collapseLineups" 
