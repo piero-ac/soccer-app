2dd035e0ca6cf71bdbef4cdc71c743d9b8893c06
Piero Coronado <pacoronado01@gmail.com>
2023-07-06 16:40:55 -0400

    Add  backend proxy to config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	build: {
+		manifest: true,
+		rollupOptions: {
+			input: "./src/main.jsx",
+		},
+	},
+	server: {
+		proxy: {
+			"/soccer": "http://localhost:3000",
+		},
+	},
+});
